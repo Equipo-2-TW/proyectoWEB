350012db67cc85530c50306909cdff8d9fdb82b9
Victor Franco <vfrancohdez1@hotmail.com>
2021-12-28 13:19:19 -0600

    feat: create and use header.php

---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="es">
+<head> <!--etiqueta de estructura básica encabezado del html-->
+    <title><?=ucfirst($title)?></title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="author" content="Equipo 2">
+    <meta name="description" content="vista usuario <?=$title?>">
+    <meta name="keywords" content="HTML, vista">
+    <meta name="copyright" content="© Equipo2">
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+    <div class="header">
+        <div class="top">
+            <div class="logo">
+                logo
+            </div>
+            <div class="titulo">
+                titulo del proyecto
+            </div>
+        </div>
