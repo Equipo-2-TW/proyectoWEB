83fedf27652cb7fc7211a3d3b46cd0dcee195958
Victor Franco <vfrancohdez1@hotmail.com>
2021-12-28 13:24:56 -0600

    feat: create and use login.php

---
+++ includes/login.php
@@ -0,0 +1,14 @@
+<div class="login">
+    Inicio de sesión
+    <form>
+        Usuario
+        <input type="text"/>
+        Contraseña
+        <input type="text"/>
+        <input type="submit" value="aceptar">
+    </form>
+    <div class="links">
+        <a href="">registrarse</a>
+        <a href="">se te olvidó la contraseña</a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -16,20 +16,7 @@
             Copyright 2021 Equipo 2 | Todos los derechos reservados
         </div>
     </div>
-    <div class="login">
-        Inicio de sesión
-        <form>
-            Usuario
-            <input type="text"/>
-            Contraseña
-            <input type="text"/>
-            <input type="submit" value="aceptar">
-        </form>
-        <div class="links">
-            <a href="">registrarse</a>
-            <a href="">se te olvidó la contraseña</a>
-        </div>
-    </div>
+    <?php require_once "includes/login.php";?>
 </div>
 </body>
 </html>
