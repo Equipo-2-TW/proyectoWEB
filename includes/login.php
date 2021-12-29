a917f2af49c3bf9f27a0f903a19f109322254024
Victor Franco <vfrancohdez1@hotmail.com>
2021-12-29 11:39:18 -0600

    feat: create sign_in.php

--- includes/login.php
+++ includes/login.php
@@ -1,16 +1,16 @@
 <div class="login">
     <div class="title">Inicio de sesión</div>
-    <form>
-        Usuario
-        <input type="text"/>
-        Contraseña
-        <input type="password"/>
+    <form method="post" action="sign_in.php">
+        <label for="login_name">Usuario</label>
+        <input type="text" name="email" id="login_name" placeholder="email"/>
+        <label for="login_password">Contraseña</label>
+        <input type="password" name="password" id="login_password"/>
         <select name="user" class="user">
             <option value="admin">Administrador</option>
             <option value="docente">Docente</option>
             <option value="alumno">Alumno</option>
         </select>
-        <input type="submit" value="aceptar"/>
+        <input type="submit" name="login" value="aceptar"/>
     </form>
     <div class="links">
         <a href="" id="registrarse">registrarse</a>
---
+++ sign_in.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "db_connection.php";
+
+if (isset($_POST["login"])) {
+    $user   = $_POST["user"];
+    $email   = $_POST["email"];
+    $pwd    = $_POST["password"];
+    switch ($user) {
+        case "admin":
+            $query = "SELECT * FROM administrador WHERE admin_correo = '$email' AND admin_contras = '$pwd'";
+            break;
+        case "docente":
+            $query = "SELECT * FROM alumno WHERE alum_correo = '$email' AND alum_contras = '$pwd'";
+            break;
+        case "alumno":
+            $query = "SELECT * FROM docente WHERE doc_correo = '$email' AND doc_contras = '$pwd'";
+            break;
+    }
+    if (isset($query)) {
+        $result = mysqli_query($conn, $query);
+        if (!$result) {
+            die("Query Failed");
+        }
+        $count = mysqli_num_rows($result);
+        if($count >= 1) {
+            header("Location: index.php");
+        }
+    }
+}
+?>
