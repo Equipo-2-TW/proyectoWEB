baf7daac65153b8f042ae1b038d6dbb80db05d0c
Victor Franco <vfrancohdez1@hotmail.com>
2021-12-29 08:49:18 -0600

    feat: create update_fields()
    Change content of modal_sign_up based on user type.

--- includes/modal_sign_up.php
+++ includes/modal_sign_up.php
@@ -2,7 +2,7 @@
     <div id="card" class="card">
         <form method="post" action="sign_up.php">
             <div id="title">Registrar:
-                <select name="user" class="user">
+                <select name="user" id="select_user_sign_up" class="user">
                     <option value="admin">Administrador</option>
                     <option value="docente">Docente</option>
                     <option value="alumno">Alumno</option>
@@ -13,7 +13,7 @@
                 <input type="text" name="name" id="name"/>
                 <label for="email">Correo:</label>
                 <input type="text" name="email" id="email"/>
-                <label for="email2">Correo Opcional:</label>
+                <label for="email2" id="email2l">Correo Opcional:</label>
                 <input type="text" name="email2" id="email2"/>
                 <label for="password">Contraseña:</label>
                 <input type="password" name="password" id="password"/>
--- js/modal_sign_up.js
+++ js/modal_sign_up.js
@@ -1,15 +1,34 @@
 const registrarse = document.querySelector("#registrarse")
-const modal = document.querySelector("#modal")
-const card = document.querySelector("#card")
+const modal   = document.querySelector("#modal")
+const card    = document.querySelector("#card")
+const select  = document.querySelector("#select_user_sign_up")
+const email2  = document.querySelector("#email2")
+const email2l = document.querySelector("#email2l")
+
+const update_fields = () => {
+    if (select.value == "admin") {
+        email2.classList.add("admin")
+        email2l.classList.add("admin")
+    }
+    else {
+        email2.classList.remove("admin")
+        email2l.classList.remove("admin")
+    }
+}
 
 document.body.addEventListener("click", () => {
     modal.classList.remove('active')
 })
+
 registrarse.addEventListener("click", e => {
     e.preventDefault()
     e.stopPropagation()
     modal.classList.toggle('active')
+    update_fields()
 })
+
 card.addEventListener("click", e => {
     e.stopPropagation()
 })
+
+select.addEventListener("change", update_fields)
