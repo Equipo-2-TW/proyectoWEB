cc64871683e67b1885da968386e2240ab2580f87
Andrea Sanchez <55214148+0draS0@users.noreply.github.com>
2022-01-01 01:52:05 -0600

    Update PROYECTO-EQ2.sql
    Adición de tablas: cita y grupo. Llaves foráneas.

--- database/PROYECTO-EQ2.sql
+++ database/PROYECTO-EQ2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost:3306
--- Tiempo de generación: 27-12-2021 a las 21:56:42
+-- Tiempo de generación: 01-01-2022 a las 01:44:55
 -- Versión del servidor: 8.0.27-0ubuntu0.20.04.1
 -- Versión de PHP: 7.4.3
 
@@ -29,10 +29,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `administrador` (
-  `admin_id` int NOT NULL,
-  `admin_nombre` varchar(18) COLLATE utf8_spanish_ci NOT NULL,
-  `admin_correo` varchar(18) COLLATE utf8_spanish_ci NOT NULL,
-  `admin_contras` varchar(9) COLLATE utf8_spanish_ci NOT NULL
+  `admin_numempleado` int NOT NULL,
+  `admin_nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `admin_correoP` varchar(35) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `admin_correoA` varchar(35) COLLATE utf8_spanish_ci NOT NULL,
+  `admin_telefono` int NOT NULL,
+  `admin_contras` varchar(8) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -42,11 +44,26 @@ CREATE TABLE `administrador` (
 --
 
 CREATE TABLE `alumno` (
-  `alum_id` int NOT NULL,
-  `alum_nombre` varchar(18) COLLATE utf8_spanish_ci NOT NULL,
-  `alum_correo` varchar(18) COLLATE utf8_spanish_ci NOT NULL,
-  `alum_correop` varchar(18) COLLATE utf8_spanish_ci NOT NULL,
-  `alum_contras` varchar(9) COLLATE utf8_spanish_ci NOT NULL
+  `alum_boleta` int NOT NULL,
+  `alum_nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `alum_grupo` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
+  `alum_correoP` varchar(35) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `alum_correoA` varchar(35) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `alum_telefono` int NOT NULL,
+  `alum_contras` varchar(8) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cita`
+--
+
+CREATE TABLE `cita` (
+  `cita_orden` int NOT NULL,
+  `cita_numempleado` int NOT NULL,
+  `cita_boleta` int NOT NULL,
+  `cita_fecha` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -56,11 +73,28 @@ CREATE TABLE `alumno` (
 --
 
 CREATE TABLE `docente` (
-  `doc_id` int NOT NULL,
-  `doc_nombre` varchar(18) COLLATE utf8_spanish_ci NOT NULL,
-  `doc_correo` varchar(18) COLLATE utf8_spanish_ci NOT NULL,
-  `doc_correop` varchar(18) COLLATE utf8_spanish_ci NOT NULL,
-  `doc_contras` varchar(9) COLLATE utf8_spanish_ci NOT NULL
+  `doc_numempleado` int NOT NULL,
+  `doc_nombre` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `doc_grupo` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
+  `doc_correoP` varchar(35) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `doc_correoA` varchar(35) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `doc_telefono` int NOT NULL,
+  `doc_contras` varchar(8) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `grupo`
+--
+
+CREATE TABLE `grupo` (
+  `grupo_docgrup` varchar(1) COLLATE utf8_spanish_ci NOT NULL,
+  `grupo_adminnum` int NOT NULL,
+  `grupo_docnum` int NOT NULL,
+  `grupo_boleta` int NOT NULL,
+  `grupo_evaluacion` int NOT NULL,
+  `grupo_observacion` text COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
@@ -71,9 +105,101 @@ CREATE TABLE `docente` (
 
 CREATE TABLE `material` (
   `mat_id` int NOT NULL,
-  `mat_nombre` varchar(12) COLLATE utf8_spanish_ci NOT NULL,
-  `mat_descrip` varchar(25) COLLATE utf8_spanish_ci NOT NULL
+  `mat_bloque` varchar(1) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `mat_tema` varchar(40) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `mat_subtema` varchar(40) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `mat_tipomaterial` varchar(40) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL,
+  `mat_actividad` varchar(50) CHARACTER SET utf8 COLLATE utf8_spanish_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `administrador`
+--
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`admin_numempleado`);
+
+--
+-- Indices de la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD PRIMARY KEY (`alum_boleta`),
+  ADD KEY `alum_grupo` (`alum_grupo`);
+
+--
+-- Indices de la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD PRIMARY KEY (`cita_orden`),
+  ADD KEY `cita_numempleado` (`cita_numempleado`,`cita_boleta`),
+  ADD KEY `cita_boleta` (`cita_boleta`);
+
+--
+-- Indices de la tabla `docente`
+--
+ALTER TABLE `docente`
+  ADD PRIMARY KEY (`doc_numempleado`),
+  ADD KEY `doc_grupo` (`doc_grupo`);
+
+--
+-- Indices de la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD KEY `grupo_docgrup` (`grupo_docgrup`,`grupo_adminnum`,`grupo_docnum`,`grupo_boleta`),
+  ADD KEY `grupo_adminnum` (`grupo_adminnum`),
+  ADD KEY `grupo_boleta` (`grupo_boleta`),
+  ADD KEY `grupo_docnum` (`grupo_docnum`);
+
+--
+-- Indices de la tabla `material`
+--
+ALTER TABLE `material`
+  ADD PRIMARY KEY (`mat_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cita`
+--
+ALTER TABLE `cita`
+  MODIFY `cita_orden` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `material`
+--
+ALTER TABLE `material`
+  MODIFY `mat_id` int NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumno`
+--
+ALTER TABLE `alumno`
+  ADD CONSTRAINT `alumno_ibfk_1` FOREIGN KEY (`alum_grupo`) REFERENCES `docente` (`doc_grupo`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `cita`
+--
+ALTER TABLE `cita`
+  ADD CONSTRAINT `cita_ibfk_1` FOREIGN KEY (`cita_numempleado`) REFERENCES `docente` (`doc_numempleado`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `cita_ibfk_2` FOREIGN KEY (`cita_boleta`) REFERENCES `alumno` (`alum_boleta`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `grupo`
+--
+ALTER TABLE `grupo`
+  ADD CONSTRAINT `grupo_ibfk_1` FOREIGN KEY (`grupo_adminnum`) REFERENCES `administrador` (`admin_numempleado`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `grupo_ibfk_2` FOREIGN KEY (`grupo_docgrup`) REFERENCES `docente` (`doc_grupo`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `grupo_ibfk_3` FOREIGN KEY (`grupo_boleta`) REFERENCES `alumno` (`alum_boleta`) ON DELETE RESTRICT ON UPDATE CASCADE,
+  ADD CONSTRAINT `grupo_ibfk_4` FOREIGN KEY (`grupo_docnum`) REFERENCES `docente` (`doc_numempleado`) ON DELETE RESTRICT ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
